5d9e6b73801c134485e5601807ba4223c84335b7
singharti <artisingh3106@gmail.com>
2019-05-01 13:06:39 +0530

    Magento files

---
+++ design/frontend/Excellence/first/registration.php
@@ -0,0 +1,8 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Excellence/first',
+    __DIR__
+);
+
---
+++ etc/env.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'd94ef9463c7347cef1de9c02ac95cd68'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'm2.2.8',
+                'username' => 'root',
+                'password' => 'java@123',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 0,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 0,
+        'reflection' => 0,
+        'db_ddl' => 0,
+        'eav' => 0,
+        'customer_notification' => 0,
+        'config_integration' => 0,
+        'config_integration_api' => 0,
+        'full_page' => 0,
+        'config_webservice' => 0,
+        'translate' => 0,
+        'vertex' => 0
+    ],
+    'install' => [
+        'date' => 'Mon, 29 Apr 2019 10:39:03 +0000'
+    ]
+];
